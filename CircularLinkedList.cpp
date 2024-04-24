205f654028f82021b44b01738db0fcdbb61c604a
bavly19 <122808015+bavly19@users.noreply.github.com>
2024-04-24 18:05:55 +0200

    Update CircularLinkedList.cpp

--- CircularLinkedList.cpp
+++ CircularLinkedList.cpp
@@ -300,3 +300,14 @@ int main(){
     cout<<c.retrieveAt(1);
 }
 
+/*
+void selection(int arr[], int n){
+    for (int i = 0; i < n-1; i++) {
+        for (int j = i+1; j < n ; j++) {
+            int min = i;
+            if (arr[j] < arr[min])
+                min = j;
+            swap(arr[min],arr[i]);
+        }
+    }
+}*/
